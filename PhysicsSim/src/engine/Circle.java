09b8a467c63c9ea427aee63f49e77a5d500fdaf8
league <league@192.168.1.85>
2026-01-17 12:25:57 -0800

    fixed bounce bugs

--- PhysicsSim/src/engine/Circle.java
+++ PhysicsSim/src/engine/Circle.java
@@ -5,32 +5,32 @@ import java.awt.Graphics;
 
 public class Circle extends PhysicsObject{
 
-	private int radius=50;
+	private int radius=25;
 	public Circle() {
 		super();
-		setPosition(400,300);
+		setPosition(400,100);
 		setVelocity(5, 0);
 	}
 	@Override
 	void draw(Graphics g) {
 		// TODO Auto-generated method stub
 		g.setColor(Color.white);
-		g.fillOval(getX(), getY(), radius, radius);
+		g.fillOval(getX(), getY(), 2*radius, 2*radius);
 		g.setColor(Color.black);
-		g.drawOval(getX(), getY(), radius, radius);
+		g.drawOval(getX(), getY(), 2*radius, 2*radius);
 	}
 	
 	void tick() {
 		super.tick();
 		//bounce check
-		if(getY()+radius>PhysicsSim.HEIGHT&&getVelocityY()>0) {
+		if(getY()+2*radius>PhysicsSim.HEIGHT&&getVelocityY()>0) {
 			//28 is the height of the window header
 			this.setVelocityY(0-getVelocityY());
-		} else if(getY()<0&&getVelocityY()<0) {
+		}  if(getY()<0&&getVelocityY()<0) {
 			this.setVelocityY(0-getVelocityY());
-		} else if(getX()+radius>PhysicsSim.WIDTH&&getVelocityX()>0) {
+		}  if(getX()+2*radius>PhysicsSim.WIDTH&&getVelocityX()>0) {
 			this.setVelocityX(0-getVelocityX());
-		} else if(getX()<0&&getVelocityX()<0) {
+		}  if(getX()<0&&getVelocityX()<0) {
 			this.setVelocityX(0-getVelocityX());
 		}
 	}
